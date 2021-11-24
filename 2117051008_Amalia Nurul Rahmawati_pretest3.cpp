447f98d6e7a78089cca70600e48efc38b97c8baf
amalianurulrahmawati <92409385+amalianurulrahmawati@users.noreply.github.com>
2021-11-24 19:48:03 +0700

    Add files via upload

---
+++ 2117051008_Amalia Nurul Rahmawati_pretest3.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main (){
+	
+		int b,t;
+	
+	cout<<"Masukan Data";
+	cin >> b,t;
+	
+	if (b ==59){
+		cout <<"Berat Badan = 59" <<endl;
+	}
+	else if(t ==1.7){
+		cout <<"Tinggi Badan = 1.7" <<endl;
+	}
+	else{
+		cout <<"Data Diri" <<endl;
+	}
+	
+	//input 
+	cout<<"Berat Badan  : "<< 59 <<endl;
+	cout<<"Tinggi Badan :"<< 1.7 <<endl;
+	
+	//output
+	cout<< 59/1.7 <<endl;
+	cout<<"Normal Weight"<<endl;
+	
+	return 0;
+
+
+
+}
